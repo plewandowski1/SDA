8c97bbe7b429ac3678dd2a0f709a1f82adbee87c
Remigiusz Zudzin <remik.zudzin@gmail.com>
2019-01-19 15:07:42 +0100

    commit1

---
+++ mavenproject2/src/main/java/pl/mavenproject2/RemigiuszZudzin.java
@@ -0,0 +1,11 @@
+package pl.mavenproject2;
+
+public class RemigiuszZudzin extends Student {
+
+    String name = "Remigiusz Zudzin";
+
+    @Override
+    String getName() {
+        return "name";
+    }
+}
